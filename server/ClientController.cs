63dbf08c0224748d981d3bb5e0591e9cd6175067
Sader Fawall <sader1992@hotmail.com>
2025-09-09 21:17:24 +0300

    c# app and license

---
+++ server/ClientController.cs
@@ -0,0 +1,46 @@
+﻿using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erioo_Gamepad_Server
+{
+    internal class ClientController
+    {
+        private IXbox360Controller controller;
+        private float prevX = 0f;
+        private float prevY = 0f;
+        private const float threshold = 0.01f;
+
+        public ClientController(IXbox360Controller c)
+        {
+            controller = c;
+        }
+
+        private bool firstJoyUpdate = true;
+
+        public void HandleJoyMessage(string msg, IXbox360Controller controller)
+        {
+            var parts = msg.Split(' ');
+            if (parts.Length != 3) return;
+            if (!float.TryParse(parts[1], out float x)) return;
+            if (!float.TryParse(parts[2], out float y)) return;
+            bool isNearZero = Math.Abs(x) < 0.1f && Math.Abs(y) < 0.1f;
+            if (firstJoyUpdate || isNearZero || Math.Abs(x - prevX) >= threshold || Math.Abs(y - prevY) >= threshold)
+            {
+                controller.SetAxisValue(Xbox360Axis.LeftThumbX.Id, (short)(x * short.MaxValue));
+                controller.SetAxisValue(Xbox360Axis.LeftThumbY.Id, (short)(y * short.MaxValue));
+
+                prevX = x;
+                prevY = y;
+                firstJoyUpdate = false;
+            }
+
+            if (isNearZero) firstJoyUpdate = true;
+        }
+    }
+
+}
---
+++ server/Program.cs
@@ -0,0 +1,91 @@
+﻿using Nefarius.ViGEm.Client;
+using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erioo_Gamepad_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9000);
+            server.Start();
+            Console.WriteLine("Server started on port 9000...");
+
+            var vigemClient = new ViGEmClient();
+            var clientControllers = new Dictionary<TcpClient, IXbox360Controller>();
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine($"Client connected! {client.Client.RemoteEndPoint.ToString()}");
+                var controller = vigemClient.CreateXbox360Controller();
+                controller.Connect();
+                clientControllers[client] = controller;
+                ClientController clientController = new ClientController(controller);
+                Task.Run(() =>
+                {
+                    using (client)
+                    {
+                        NetworkStream stream = client.GetStream();
+                        byte[] buffer = new byte[1024];
+                        while (true)
+                        {
+                            try
+                            {
+                                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                                if (bytesRead == 0) break;
+                                string chunk = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                                var lines = chunk.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                                foreach (var line in lines)
+                                {
+                                    ///Console.WriteLine($"Client {client.Client.RemoteEndPoint}: {line}");
+                                    HandleInput(controller, line, clientController);
+                                }
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                        }
+                        Console.WriteLine("Client disconnected.");
+                        controller.Disconnect();
+                        clientControllers.Remove(client);
+                    }
+                });
+            }
+        }
+
+        static void HandleInput(IXbox360Controller controller, string msg, ClientController clientController)
+        {
+            switch (msg)
+            {
+                case "A": controller.SetButtonState(Xbox360Button.A, true); break;
+                case "A_UP": controller.SetButtonState(Xbox360Button.A, false); break;
+                case "B": controller.SetButtonState(Xbox360Button.B, true); break;
+                case "B_UP": controller.SetButtonState(Xbox360Button.B, false); break;
+                case "X": controller.SetButtonState(Xbox360Button.X, true); break;
+                case "X_UP": controller.SetButtonState(Xbox360Button.X, false); break;
+                case "Y": controller.SetButtonState(Xbox360Button.Y, true); break;
+                case "Y_UP": controller.SetButtonState(Xbox360Button.Y, false); break;
+                case "START": controller.SetButtonState(Xbox360Button.Start, true); break;
+                case "START_UP": controller.SetButtonState(Xbox360Button.Start, false); break;
+                case "BACK": controller.SetButtonState(Xbox360Button.Back, true); break;
+                case "BACK_UP": controller.SetButtonState(Xbox360Button.Back, false); break;
+                default:
+                    if (msg.StartsWith("JOY"))
+                    {
+                        clientController.HandleJoyMessage(msg, controller);
+                    }
+                    break;
+            }
+            controller.SubmitReport();
+        }
+    }
+}
